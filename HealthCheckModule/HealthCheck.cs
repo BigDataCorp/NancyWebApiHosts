ef9d3b6dda5a5137e8236ba4ff915bc5acafa58a
Khalid Salomão <khalidsalomao@gmail.com>
2015-09-08 18:37:48 -0300

    Health check module

---
+++ HealthCheckModule/HealthCheck.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using NancyApiHost.Security;
+using System;
+using System.Linq;
+
+namespace HealthCheckModule
+{
+    public class HealthCheck : NancyModule
+    {
+        public HealthCheck ()
+            : base ("/HealthCheck")
+        {
+            Get["/"] = p => "Ok";
+        }
+    }
+}
